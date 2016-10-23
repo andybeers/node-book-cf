d8869f0b87453f5ddf51f98a0fe0a2b138a30b6e
Andrew Beers <andybeers@gmail.com>
2016-10-23 15:09:20 -0500

    server module exports

--- server.js
+++ server.js
@@ -6,5 +6,16 @@ var http = require('http');
 //   response.end();
 // }).listen(8888);
 
+function start() {
+  function onRequest(request, response) {
+    console.log('Request recieved.');
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello World');
+    response.end();
+  }
 
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started.');
+}
 
+exports.start = start;
