46f93da6e6f2eeaa6f954953686bf5b119dbd38c
Andrew Beers <andybeers@gmail.com>
2016-10-23 17:04:18 -0500

    Added more handlers, handle object to index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var server = require('./server');
 var router = require('./router');
+var requestHanlders = require('./requestHandlers');
+
+var handle = {};
+handle['/'] = requestHanlders.start;
+handle['/start'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
 
 server.start(router.route);
